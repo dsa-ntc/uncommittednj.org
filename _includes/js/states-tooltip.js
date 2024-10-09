27195e8fbd654d5ce98693a97641091092a47942
Fernando Gonzalez <fdgonzalez781@gmail.com>
2024-10-09 05:34:05 -0400

    Try to fix logo tooltip

---
+++ _includes/js/states-tooltip.js
@@ -0,0 +1,43 @@
+var tooltipSpan = document.getElementById('details-box');
+var stateList = {{ site.data.states | json | replace: '=>', ':'}}
+console.log(stateList);
+console.log("hi");
+
+/*        window.addEventListener("load", (e) => {
+          var els = document.querySelectorAll(".swing");
+          els.forEach((el) => {
+            var wrapper = document.createElement("a");
+            wrapper.setAttribute("href", stateList[el.classList[0]].url);
+            el.parentNode.insertBefore(wrapper, el);
+            wrapper.appendChild(el);
+          });
+        });
+*/
+
+document.addEventListener('mouseover', function (e) {
+    var stateAbbrev = e.target.classList[0];
+    if (e.target.tagName == 'path' && e.target.classList[1] == "swing") {
+        if (tooltipSpan.firstChild != null) {
+          tooltipSpan.removeChild(tooltipSpan.firstChild); 
+        }
+        var state = stateList[stateAbbrev];
+        var petitionImg = document.createElement("img");
+        var petitionImgUrl = "/assets/" + state.img;
+
+        petitionImg.setAttribute("src", petitionImgUrl);
+        petitionImg.setAttribute("height", "100px");
+
+        tooltipSpan.appendChild(petitionImg);
+        tooltipSpan.style.opacity = "100%";
+    }
+    else {
+        tooltipSpan.style.opacity = "0%";
+    }
+});
+
+window.onmousemove = function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+    tooltipSpan.style.top = (y + 20) + 'px';
+    tooltipSpan.style.left = (x) + 'px';
+};
